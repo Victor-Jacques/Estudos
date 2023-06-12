c4db53753821e5aea0e6e4d825ccccded63644a4
Victor <victordjacques@gmail.com>
2023-06-12 16:27:08 -0300

    temperature in python

---
+++ Python/temp.py
@@ -0,0 +1,8 @@
+try:
+    celsius = int(input('Enter temperature value in celsius(integer only): '))
+    fahrenheit = celsius * (9/5) + 32
+    kelvin = celsius + 273.15
+    print('Temperature in fahrenheit is: ', fahrenheit)
+    print('Temperature in kelvin is: ', kelvin)
+except:
+    print('Invalid input')

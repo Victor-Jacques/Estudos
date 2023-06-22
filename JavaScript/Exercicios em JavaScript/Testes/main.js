08e6cdba04ee7644d957ed22682c301afe5d844e
Victor <victordjacques@gmail.com>
2023-06-21 21:03:49 -0300

    commit

--- JavaScript/Exercicios em JavaScript/Testes/main.js
+++ JavaScript/Exercicios em JavaScript/Testes/main.js
@@ -6,3 +6,22 @@
   }
 };*/
 
+/*function myFunction (a, b){
+  return a.includes(b) ? b + a : a + b
+  return a.indexOf(b) === -1 ? a + b : b + a
+}*/
+
+/*function myFunction(a){
+  if(a % 2 === 0){
+    return true
+  } else {
+    return false
+  }
+}*/
+
+/*function myFunction(a, b){
+  return b.split(a).length - 1
+}*/
+
+
+console.log(myFunction('', ''));

80f387211599f33eebac0e20f46ba50a3fc99344
Victor <victordjacques@gmail.com>
2023-08-03 14:04:49 -0300

    commit

--- JavaScript/Alura/for.js
+++ JavaScript/Alura/for.js
@@ -1,5 +1,5 @@
 const numbers = [100, 200, 300, 400, 500, 600];
 
-for (i=0, i<numbers.length, i++) {
-    console.log(numbers)
+for (let i=0; i<numbers.length; i++) {
+    console.log(numbers[i]);
 }

cfd4ef88ea0ec435d2b22e3d12abf4bda24ca6f1
Victor <victordjacques@gmail.com>
2023-08-04 14:32:17 -0300

    commit

--- Java/src/Alura/BankAccount.java
+++ Java/src/Alura/BankAccount.java
@@ -10,16 +10,16 @@ public class BankAccount {
         float saldoCliente1 = 2500;
         int operacoes = 0;
 
-        System.out.println("************************");
-        System.out.println("Dados iniciais do cliente: \n");
+        System.out.println("************************ \n");
+        System.out.println("Dados iniciais do cliente:");
         System.out.println("Nome: " + cliente1);
         System.out.println("Tipo de conta: " + tipoDeConta);
         System.out.printf("Saldo inicial: R$ %.2f \n", saldoCliente1);
-        System.out.println("************************");
+        System.out.println("\n ************************");
 
         while (operacoes != 4) {
 
-            System.out.println("\n Operações");
+            System.out.println("\n   Operações");
             System.out.println("1- Consultar saldos");
             System.out.println("2- Receber valor");
             System.out.println("3- Transferir valor");
@@ -28,12 +28,23 @@ public class BankAccount {
             operacoes = scan.nextInt();
 
             if (operacoes == 1) {
-                System.out.printf("O saldo da conta é de: R$ %.2f", saldoCliente1);
+                System.out.printf("O saldo da conta é de: R$ %.2f \n", saldoCliente1);
             } else if (operacoes == 2) {
                 System.out.println("Insira o valor à receber: ");
                 float recebeValor = scan.nextFloat();
                 saldoCliente1 += recebeValor;
-                System.out.printf("O novo saldo da conta é de: R$ %.2f", saldoCliente1);
+                System.out.printf("O novo saldo da conta é de: R$ %.2f \n", saldoCliente1);
+            } else if (operacoes == 3) {
+                System.out.println("Insira o valor da transferência:");
+                float valorTransferencia = scan.nextFloat();
+                if (saldoCliente1 < valorTransferencia) {
+                    System.out.println("Saldo insuficiente para efetuar a transferência.");
+                } else {
+                    saldoCliente1 -= valorTransferencia;
+                    System.out.printf("Transferência no valor de R$ %.2f efetuada com sucesso. O novo saldo da conta é de: R$ %.2f", valorTransferencia, saldoCliente1);
+                }
+            } else if (operacoes == 4) {
+                System.out.println("Saindo do sistema.");
             }
         }
     }

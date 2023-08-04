18e24ee5ae0fbf81b43fe6a4dc67f57dd9b296c6
Victor <victordjacques@gmail.com>
2023-08-04 00:48:01 -0300

    commit

--- JavaScript/Alura/mediafor.js
+++ JavaScript/Alura/mediafor.js
@@ -28,14 +28,38 @@ console.log(`A média das notas é ${media}.`) */
 // media = media/notasGerais.length
 // console.log(media)
 
-const notas = [10, 6.5, 8, 7.5]
 
-let somaDasNotas = 0
 
-for(let i of notas){
-     somaDasNotas += i
- }
+// const notas = [10, 6.5, 8, 7.5]
 
-let media = somaDasNotas / notas.length
+// let somaDasNotas = 0
 
-console.log(`A média das notas é ${media}`);
+// for(let i of notas){
+//      somaDasNotas += i
+//  }
+
+// let media = somaDasNotas / notas.length
+
+// console.log(`A média das notas é ${media}`);
+
+
+
+// spread operator
+
+// const notas = [7, 7, 8, 9]
+
+// const novasNotas = [...notas]  = [7, 7, 8, 9]
+// const novasNotas = [...notas, 10]  = [7, 7, 8, 9, 10]
+// const novasNotas = [5, ...notas, 10]  = [5, 7, 7, 8, 9, 10]
+// novasNotas.push(10);
+// novasNotas = [7, 7, 8, 9, 10]  notas = [7, 7, 8, 9]
+
+
+
+const notas = [10, 9.5, 8, 7, 6];
+
+const notasAtualizadas = notas.map((nota)=>{
+    return nota + 1 >= 10 ? 10 : nota + 1
+});
+
+console.log(notasAtualizadas)

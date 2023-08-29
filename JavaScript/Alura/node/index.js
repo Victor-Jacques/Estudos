ed7270f02de5d87a291ea1da768e781d6ddd858e
Victor <victordjacques@gmail.com>
2023-08-29 16:17:48 -0300

    commit

--- JavaScript/Alura/node/index.js
+++ JavaScript/Alura/node/index.js
@@ -1,4 +1,35 @@
-import chalk from 'chalk';
+import fs from 'fs'; //Nativo do node
+import chalk from 'chalk'; //Não é nativo do node
+import { log } from 'console';
 
-console.log(chalk.blue("Hello world!"));
-console.log(chalk.red("Hello world!"));
+
+function trataErro(erro) {
+    throw new Error (chalk.red(erro.code, 'Não há arquivos no diretório.'));
+}
+
+// async/await
+
+async function pegaArquivo (caminhoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding)
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+// promises com then()
+
+// function pegaArquivo(caminhoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
+// }
+
+
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/')

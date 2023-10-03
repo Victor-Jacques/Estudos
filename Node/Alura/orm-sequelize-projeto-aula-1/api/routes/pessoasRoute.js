9cfb23a22d992cfe0148048fb40fd9cc61db1fe9
Victor <victordjacques@gmail.com>
2023-10-03 16:52:21 -0300

    commit

--- Node/Alura/orm-sequelize-projeto-aula-1/api/routes/pessoasRoute.js
+++ Node/Alura/orm-sequelize-projeto-aula-1/api/routes/pessoasRoute.js
@@ -5,9 +5,12 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
 router.post('/pessoas', PessoaController.criaPessoa);
-router.put('/pessoas/:id', PessoaController.atualizaPessoa)
-router.delete('/pessoas/:id', PessoaController.apagaPessoa
-)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router;

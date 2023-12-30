f812fcb7948c7902b9e201fa84000282aea95b72
Victor <victordjacques@gmail.com>
2023-12-30 20:40:03 -0300

    Exercicios adicionados a uma pasta

--- Python/BD.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(database = " postgres", user="postgres" , password = "senha123" , host= "127.0.0.1", port = "5432") 
-print ("Conexão com o Banco de Dados aberta com sucesso!") 
-cur=conn.cursor() 
-cur.execute('''CREATE TABLE PRODUTO(CODIGO INT PRIMARY KEY NOT NULL, NOME TEXT NOT NULL, PRECO REAL NOT NULL);''')
-print ("Tabela criada com sucesso!")
-conn.commit()
-conn.close()
